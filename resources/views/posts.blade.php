f5538955943777d2d9c4dbd603b7e65b10fa97e4
mohamedAyman <modyayman2001@gmail.com>
2023-10-29 19:44:13 +0200

    Initial commit

---
+++ resources/views/posts.blade.php
@@ -0,0 +1,12 @@
+<x-layout>
+    <article>
+        @foreach ($posts as $post)
+            <h1>
+                <a href="/posts/{{ $post->slug }}"> {!! $post->title !!} </a>
+            </h1>
+            <div>
+                <?= $post->excerpt ?>
+            </div>
+        @endforeach
+    </article>
+</x-layout>
