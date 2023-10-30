44c1dd0df71f6dc971515c888d27774ba07478f6
mohamedAyman <modyayman2001@gmail.com>
2023-10-30 23:08:13 +0200

    Show Posts by Category

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -2,7 +2,7 @@
     <article>
         <h1>{!! $post->title !!} </h1>
         <p>
-            <a href="#">{{ $post->category->name }}</a>
+            <a href="{{ $post->category->slug}}">{{ $post->category->name }}</a>
         </p>
         <div>{!! $post->body !!} </div>
     </article>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Category;
 use App\Models\post;
 
 use Illuminate\Support\Facades\Route;
@@ -29,6 +30,11 @@ Route::get('posts/{post:slug}', function (Post $post) {
         'post' => $post
     ]);
 
+});
+Route::get('categories/{category:slug}', function (Category $category) {
+    return view('posts', [
+        'posts' => $category->posts
+    ]);
 });
 
 
