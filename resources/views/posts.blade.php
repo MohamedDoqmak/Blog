63f8586dc866fe75e76c56acfef1bd55631855b1
mohamedAyman <modyayman2001@gmail.com>
2023-10-30 21:05:34 +0200

    Implement Eloquent between Post and Category

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -2,11 +2,19 @@
     <article>
         @foreach ($posts as $post)
             <h1>
-                <a href="/posts/{{ $post->slug }}"> {!! $post->title !!} </a>
+                <a href="/posts/{{ $post->slug }}">
+                    {!! $post->title !!}
+                </a>
             </h1>
+
+            <p>
+                <a href="#">{{ $post->category->name }}</a>
+            </p>
+
             <div>
                 <?= $post->excerpt ?>
             </div>
+            <hr>
         @endforeach
     </article>
 </x-layout>
