08f5cd387c6cbb82212c520d3bac347ca3bf978b
mohamedAyman <modyayman2001@gmail.com>
2023-10-31 21:17:21 +0200

    Eager Load with exisiting Model

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -7,7 +7,7 @@
                 </a>
             </h1>
             <p>
-                by <a href="authors/{{$post->author->username}}">{{$post->author->name}}</a> in <a href="{{ $post->category->slug}}">{{ $post->category->name }}</a>
+                by <a href="authors/{{$post->author->username}}">{{$post->author->name}}</a> in <a href="categories/{{ $post->category->slug}}">{{ $post->category->name }}</a>
             </p>
             <div>
                 <?= $post->excerpt ?>
