4ec01d00eef0a9bb986d69c7692bc7507a951024
mohamedAyman <modyayman2001@gmail.com>
2023-11-01 00:14:44 +0200

    HTML and CSS into Blade

--- resources/views/posts.blade.php
+++ resources/views/posts.blade.php
@@ -1,5 +1,5 @@
 <x-layout>
-    <article>
+    {{-- <article>
         @foreach ($posts as $post)
             <h1>
                 <a href="/posts/{{ $post->slug }}">
@@ -14,5 +14,23 @@
             </div>
             <hr>
         @endforeach
-    </article>
+    </article> --}}
+
+    @include('_posts-header')
+
+    <main class="max-w-6xl mx-auto mt-6 lg:mt-20 space-y-6">
+        <x-post-featured-card />
+
+        <div class="lg:grid lg:grid-cols-2">
+            <x-post-card />
+            <x-post-card />
+
+        </div>
+
+        <div class="lg:grid lg:grid-cols-3">
+            <x-post-card />
+            <x-post-card />
+            <x-post-card />
+        </div>
+    </main>
 </x-layout>
