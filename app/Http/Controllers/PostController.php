936b20ccfd8a6fa246687d1f8e5a7b2f3eec2700
mohamedAyman <modyayman2001@gmail.com>
2023-11-08 22:39:29 +0200

    Eloquent query bug Fixed

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,7 +12,11 @@ class PostController extends Controller
     public function index()
     {
         return view('posts.index', [
-            'posts' => Post::latest()->filter(request(['search', 'category', 'author']))->get()
+            'posts' => Post::latest()->filter(
+                request(
+                    ['search', 'category', 'author']
+                )
+            )->get()
         ]);
     }
     public function show(Post $post)
