4197295cc6e6e0829c60b2f549e88e64206af545
mohamedAyman <modyayman2001@gmail.com>
2023-11-08 23:29:08 +0200

    Pagination

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -12,16 +12,14 @@ class PostController extends Controller
     public function index()
     {
         return view('posts.index', [
-            'posts' => Post::latest()->filter(
-                request(
-                    ['search', 'category', 'author']
-                )
-            )->get()
+            'posts' => Post::latest()->
+                filter(
+                    request(['search', 'category', 'author'])
+                )->paginate(6)->withQueryString()
         ]);
     }
     public function show(Post $post)
     {
-
         return view('posts.show', [
             'post' => $post
         ]);
